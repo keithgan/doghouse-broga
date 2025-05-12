037a5b36c434ff7ff22a0586a52c9e4856e4dd88
keithgan <keithgan96@gmail.com>
2025-05-12 20:30:22 +0800

    fix: use correct vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    base: '/build/',
+    build: {
+        outDir: 'public_html/build', // 👈 send to cPanel root
+        emptyOutDir: true,
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
