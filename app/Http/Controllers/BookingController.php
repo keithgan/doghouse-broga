251ac20acbcbce14d566fcd5aa01e223122dd5e6
keithgan <keithgan96@gmail.com>
2025-06-22 22:56:43 +0800

    fix: send to correct email

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -82,8 +82,8 @@ class BookingController extends Controller
         Mail::to($booking['email'])->queue((new BookingConfirmation($booking))->from('enquiries@doghousebroga.com', 'Doghouse Broga'));
         
         // Send confirmation email to admin
-        // Mail::to(['enquiries@doghousebroga.com', 'doghousebroga@yahoo.com'])->queue((new Booking($booking))->from('enquiries@doghousebroga.com', 'Doghouse Broga'));
-        Mail::to('keithgan96@gmail.com')->queue((new Booking($booking))->from('enquiries@doghousebroga.com', 'Doghouse Broga'));
+        Mail::to(['enquiries@doghousebroga.com', 'doghousebroga@yahoo.com'])->queue((new Booking($booking))->from('enquiries@doghousebroga.com', 'Doghouse Broga'));
+        // Mail::to('keithgan96@gmail.com')->queue((new Booking($booking))->from('enquiries@doghousebroga.com', 'Doghouse Broga'));
     
         return redirect()->route('thank-you')->with('success', 'Booking submitted successfully!');
     }
